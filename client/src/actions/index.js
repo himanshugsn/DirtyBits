35d9342842b40d9574e74dc771bac9602f6a160b
mohitb07 <bmohit980@gmail.com>
2021-05-16 13:19:15 +0530

    redux added

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
-import { FETCH_USER } from './types'
+import { FETCH_USER, UPDATE_USER } from './types'
 
 export const fetchUser = () => async dispatch => {
     const res = await axios.get('/api/current_user');
 
     dispatch({
         type: FETCH_USER,
+        payload : res.data
+    })
+}
+
+export const sendData = (data) => async dispatch => {
+    const res = await axios.post('/api/update_user', data);
+
+    dispatch({
+        type: UPDATE_USER,
         payload : res.data
     })
 }
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,9 +1,11 @@
-import { FETCH_USER } from '../actions/types'
+import { FETCH_USER, UPDATE_USER } from '../actions/types'
 
 export default function(state = null, action) {
     switch(action.type){
         case FETCH_USER:
             return action.payload || false;
+        case UPDATE_USER:
+            return {...state }
         default:
             return state;
     }
