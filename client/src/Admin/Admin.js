96624e0e4a31453e8f8ae844d63d7ef3f9982b02
mohitb07 <bmohit980@gmail.com>
2021-05-23 20:22:48 +0530

    time series data added to the state

--- client/src/Admin/Admin.js
+++ client/src/Admin/Admin.js
@@ -15,7 +15,8 @@ class Admin extends React.Component {
             series:[0,0,0],
             rank:0,
             solved:0,
-            partiallySolved:0
+            partiallySolved:0,
+            data:[{}]
         }
     }
 
@@ -25,14 +26,16 @@ class Admin extends React.Component {
             // console.table(response.data.userDetail)
             // const newSeries = Object.values(response.data.userDetail)  
             // console.log(newSeries)          
-            console.log(response.data)
             const solved = response.data.solvedQuestion.length
-            console.log('length of solved array', solved)
+            // console.log('length of solved array', solved)
+            // console.log('attempted', response.data.attemptedQuestions)
+            // console.log(response.data.timeSeriesGraphData)
             this.setState({
-                series : [solved, response.data.partiallySolvedQuestion.length, response.data.attemptedQuestions],
+                series : [solved, response.data.partiallySolvedQuestion.length, response.data.attemptedQuestions.length],
                 rank:response.data.rank,
                 solved:solved,
-                partiallySolved:response.data.partiallySolvedQuestion.length
+                partiallySolved:response.data.partiallySolvedQuestion.length,
+                data: response.data.timeSeriesGraphData
             })
 
           }).catch(error => {
@@ -50,27 +53,31 @@ class Admin extends React.Component {
     }
 
     render(){
+        if(this.state.data.length > 0) {
+            var { data } = this.state;
+            console.log('data', data)
+        }
         return (
             <>
             {
-                this.props.auth ? 
+                this.props.auth && this.state.data.length > 0 ? 
                 <>
-                <h1 className={styles.dashboardTitle}>Your Dashboard</h1>
+                <h1 className={cx(styles.dashboardTitle, styles.heading1)}>Your Dashboard</h1>
                 <div className={cx(styles.container)}>
-                <LineGraph/>
+                <LineGraph data={data}/>
                 <div className={styles.cardsPieContainer}>
                     <div className={styles.cardsContainer}>
                         <div className={styles.card}>
-                            <h4>Rank</h4>
-                            <span><h1>{this.state.rank}</h1></span>
+                            <h4 className={styles.heading4}>Rank</h4>
+                            <span><h1 style={{marginTop:'26%'}} className={styles.heading1}>{this.state.rank}</h1></span>
                         </div>
                         <div className={cx(styles.card, styles.card2)}>
-                            <h4>Partially solved</h4>
-                            <span><h1>{this.state.partiallySolved}</h1></span>
+                            <h4 className={styles.heading4}>Partially solved</h4>
+                            <span><h1 style={{marginTop:'26%'}} className={styles.heading1}>{this.state.partiallySolved}</h1></span>
                         </div>
                         <div className={cx(styles.card, styles.card3)}>
-                            <h4>Problem solved</h4>
-                            <span><h1>{this.state.solved}</h1></span>
+                            <h4 className={styles.heading4}>Problem solved</h4>
+                            <span><h1 style={{marginTop:'26%'}} className={styles.heading1}>{this.state.solved}</h1></span>
                         </div>
                     </div>
                         <Pie series={this.state.series}/>
@@ -80,7 +87,7 @@ class Admin extends React.Component {
             </> 
             :
             <>
-            <div style={{}}>
+            <div>
                 <Alert/>
                 <div style={{ marginTop:'20px', display:'flex', justifyContent:'center'}}>
                     <Login/>
