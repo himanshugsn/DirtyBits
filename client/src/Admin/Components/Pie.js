aaa43af7f852f5066824e85485163ced8de57ce3
mohitb07 <bmohit980@gmail.com>
2021-05-23 20:20:04 +0530

    new pie added

--- client/src/Admin/Components/Pie.js
+++ client/src/Admin/Components/Pie.js
@@ -1,59 +1,78 @@
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+// import React from 'react';
+// import ReactApexChart from 'react-apexcharts';
 
-      class Pie extends React.Component {
-        constructor(props) {
-          super(props);
+//       class Pie extends React.Component {
+//         constructor(props) {
+//           super(props);
 
-          this.state = {
+//           this.state = {
           
-            series: this.props.series,
-            options: {
-              chart: {
-                width: 380,
-                type: 'pie',
-              },
-              labels: ['Solved', 'Partially', 'Attempted'],
-              responsive: [{
-                breakpoint: 480,
-                options: {
-                  chart: {
-                    width: 200
-                  },
-                  legend: {
-                    position: 'bottom'
-                  }
-                }
-              }]
-            },
+//             series: this.props.series,
+//             options: {
+//               chart: {
+//                 width: 380,
+//                 type: 'pie',
+//               },
+//               labels: ['Solved', 'Partially', 'Attempted'],
+//               responsive: [{
+//                 breakpoint: 480,
+//                 options: {
+//                   chart: {
+//                     width: 200
+//                   },
+//                   legend: {
+//                     position: 'bottom'
+//                   }
+//                 }
+//               }]
+//             },
           
           
-          };
-        }
+//           };
+//         }
 
-        // dataHandler(){
-        //   if(this.state.series){
-        //     return (
-        //       <div id="chart">
-        //       <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />
-        //     </div>
-        //     )
-        //   } else {
-        //     return (
-        //       <div>
-        //         <h6>Not enough data</h6>
-        //       </div>
-        //     )
-        //   }
-        // }
+//         // dataHandler(){
+//         //   if(this.state.series){
+//         //     return (
+//         //       <div id="chart">
+//         //       <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />
+//         //     </div>
+//         //     )
+//         //   } else {
+//         //     return (
+//         //       <div>
+//         //         <h6>Not enough data</h6>
+//         //       </div>
+//         //     )
+//         //   }
+//         // }
 
-        render() {
-          return (
-            <div id="chart">
-            <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />
-          </div>
-          )
-        }
-      }
+//         render() {
+//           return (
+//             <div id="chart">
+//             <ReactApexChart options={this.state.options} series={this.state.series} type="pie" width={380} />
+//           </div>
+//           )
+//         }
+//       }
 
-export default Pie;
+// export default Pie;
+
+import { PieChart } from 'reaviz';
+
+const MyChart = (props) => (
+  props.series.length > 0 &&
+  (
+  <PieChart
+    height={300}
+    width={300}
+    data={[
+      { key: 'Solved', data: props.series[0] },
+      { key: 'Partially', data: props.series[1] },
+      { key: 'Attempted', data: props.series[2] },
+    ]}
+  />
+  )
+);
+
+export default MyChart;
