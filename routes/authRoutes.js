e69300d0ac682a1f8621d0b81fc3da8b2ca97089
mohitb07 <bmohit980@gmail.com>
2021-05-25 21:48:10 +0530

    filtered user who have alteast one solved question

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -19,49 +19,33 @@ module.exports = (app) => {
     })
     
     app.get('/api/current_user', async(req, res)=>{
-        // const users = await User.find({});
-        // console.log(users.sort((a,b)=>a-b));
         res.send(req.user)
     })
 
     app.get('/api/leaderboard', requireLogin,  async(req, res) => {
-        const users = await User.find({});
+        const users = await User.find({
+            'solvedQuestion.0' :{"$exists":true} 
+        });
         console.log('before sort', users)
-        users.sort(function(a, b) {
-            var keyA = a.problemsSolved, keyB = b.problemsSolved;
-            if(keyA < keyB) return -1;
-            if(keyA > keyB) return 1;
+        const sortedUser = users.sort(function(a, b) {
+            var keyA = a.solvedQuestion.length, keyB = b.solvedQuestion.length;
+            if(keyA > keyB) return -1;
+            if(keyA < keyB) return 1;
             return 0;
         })
         console.log('after sort', users)
         console.log('my current id ', req.user._id)
-        const getObj = users.find((x) => x.email === req.user.email)
-        const rank = users.indexOf(getObj) + 1;
+        const getObj = sortedUser.find((x) => x.email === req.user.email)
+        const rank = sortedUser.indexOf(getObj) + 1;
         req.user.rank = rank;
         const user = await req.user.save()
-        res.json(users)
+        res.json(sortedUser)
     })
 
 
 
     app.post('/api/update_user', requireLogin, async (req, res) => {
         const { score, problemId } = req.body;
-        // console.log('score value', score);
-        // console.log('reached');
-        // console.log('problem id is ', problemId);
-        // const user = await User.findByIdAndUpdate(req.body.userId, {$set:{score:10,attempted : 1 }}, (err, db) => {
-        //     if (err) throw err;
-        //     console.log('1 document updated')
-        // })
-        // const newUser = await user.save()
-
-        // if(!req.user.attemptedQuestions.includes(problemId)){
-        //     const user = await User.findByIdAndUpdate(req.body.userId, {$push : {attemptedQuestions : problemId}})
-        //     const updateUser = await user.save();
-        //     res.status(203).send(updateUser)
-        // }
-
-        // res.status(200).send(newUser)
 
         try {
             if(score === 100 ) {
