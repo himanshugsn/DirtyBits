30d732834bf6a3e91c03128e82a0be9d5e825d3c
mohitb07 <bmohit980@gmail.com>
2021-06-09 20:47:29 +0530

    lazy loading the compoenents

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, lazy } from "react";
 import {Route} from 'react-router-dom';
 import {Router} from 'react-router';
 
-import ProblemView from "./Components/ProblemView/ProblemView";
-import ProblemList from './Components/ProbemList/ProblemList';
-import Navbar from './Components/ProblemView/Navbar';
-import ProblemLevel from './Components/ProblemLevel/ProblemLevel';
-import settings from './settings';
-import logout from './logout';
-import history from './history';
-import Admin from "./Admin/Admin";
-import profile from "./Components/Profile/profile";
-
 import * as actions from './actions'
+import { Suspense } from "react";
 import { connect } from "react-redux";
-import Leaderboard from "./Components/Leaderboard/Leaderboard";
+import history from './history';
+import Loader from './Loader';
+// import ProblemView from "./Components/ProblemView/ProblemView";
+const ProblemView = lazy(()=> import('./Components/ProblemView/ProblemView'));
+// import ProblemList from './Components/ProbemList/ProblemList';
+const ProblemList = lazy(()=> import('./Components/ProbemList/ProblemList'))
+// import Navbar from './Components/ProblemView/Navbar';
+const Navbar = lazy(()=> import('./Components/ProblemView/Navbar'))
+// import ProblemLevel from './Components/ProblemLevel/ProblemLevel';
+const ProblemLevel = lazy(()=> import('./Components/ProblemLevel/ProblemLevel'))
+// import settings from './settings';
+const settings = lazy(()=> import('./settings'))
+// import logout from './logout';
+const logout = lazy(()=> import('./logout'))
+// import Admin from "./Admin/Admin";
+const Admin = lazy(()=> import('./Admin/Admin'))
+// import profile from "./Components/Profile/profile";
+const profile = lazy(() => import('./Components/Profile/profile'))
 
+// import Leaderboard from "./Components/Leaderboard/Leaderboard";
+const Leaderboard = lazy(()=> import('./Components/Leaderboard/Leaderboard'))
 
 class App extends Component {
 
@@ -27,6 +37,7 @@ class App extends Component {
     return (
       <>
       <Router history={history} >
+        <Suspense fallback={<Loader/>}>
         <div style={{overflowX:'hidden'}}>
           <Navbar/>
           <Route path="/logout" component={logout}/>
@@ -44,6 +55,7 @@ class App extends Component {
           <Route path="/profile" exact component={profile}/>
           <Route path="/leaderboard" exact component={Leaderboard}/>
         </div>
+        </Suspense>
       </Router>
     </>
     )
