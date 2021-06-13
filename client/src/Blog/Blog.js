e2ab0c413a22eb49a32ea67634609a9c873b58d1
mohitb07 <bmohit980@gmail.com>
2021-06-13 21:32:51 +0530

    blog added

--- client/src/Blog/Blog.js
+++ client/src/Blog/Blog.js
@@ -8,7 +8,6 @@ import FacebookIcon from '@material-ui/icons/Facebook';
 import TwitterIcon from '@material-ui/icons/Twitter';
 import MainFeaturedPost from './MainFeaturedPost';
 import FeaturedPost from './FeaturedPost';
-import Sidebar from './Sidebar';
 import post1 from './blog-post.1.md';
 import post2 from './blog-post.2.md';
 import post3 from './blog-post.3.md';
@@ -20,58 +19,33 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const mainFeaturedPost = {
-  title: 'Title of a longer featured blog post',
+  title: 'React JS VS Vue JS',
   description:
-    "Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.",
-  image: 'https://source.unsplash.com/random',
+    "The primary difference lies in the methods used by Vue vs. React for rendering content onto the DOM.",
+  image: 'https://www.valuecoders.com/blog/wp-content/uploads/2019/03/1_WRzDZndJCduHwqgOpWmbhQ.png',
   imgText: 'main image description',
   linkText: 'Continue reading…',
 };
 
 const featuredPosts = [
   {
-    title: 'Featured post',
-    date: 'Nov 12',
+    title: 'Node Js API',
+    date: 'Jun 10',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+      'The Express framework, and MongoDB, focusing on the fundamental REST routes and basic database interaction. ',
+    image: 'https://bs-uploads.toptal.io/blackfish-uploads/blog/post/seo/og_image_file/og_image/15921/secure-rest-api-in-nodejs-18f43b3033c239da5d2525cfd9fdc98f.png',
     imageText: 'Image Text',
   },
   {
-    title: 'Post title',
-    date: 'Nov 11',
+    title: 'Kubernetes',
+    date: 'May 11',
     description:
-      'This is a wider card with supporting text below as a natural lead-in to additional content.',
-    image: 'https://source.unsplash.com/random',
+      'Kubernetes is an open-source container-orchestration system for automating computer application deployment, scaling, and management.',
+    image: 'https://analyticsindiamag.com/wp-content/uploads/2020/12/Kubernetes_AIM.jpg',
     imageText: 'Image Text',
   },
 ];
 
-const posts = [post1, post2, post3];
-
-const sidebar = {
-  title: 'About',
-  description:
-    'Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.',
-  archives: [
-    { title: 'March 2020', url: '#' },
-    { title: 'February 2020', url: '#' },
-    { title: 'January 2020', url: '#' },
-    { title: 'November 1999', url: '#' },
-    { title: 'October 1999', url: '#' },
-    { title: 'September 1999', url: '#' },
-    { title: 'August 1999', url: '#' },
-    { title: 'July 1999', url: '#' },
-    { title: 'June 1999', url: '#' },
-    { title: 'May 1999', url: '#' },
-    { title: 'April 1999', url: '#' },
-  ],
-  social: [
-    { name: 'GitHub', icon: GitHubIcon },
-    { name: 'Twitter', icon: TwitterIcon },
-    { name: 'Facebook', icon: FacebookIcon },
-  ],
-};
 
 export default function Blog() {
   const classes = useStyles();
@@ -89,12 +63,6 @@ export default function Blog() {
           </Grid>
           <Grid container spacing={5} className={classes.mainGrid}>
             {/* <Main title="From the firehose" posts={posts} /> */}
-            <Sidebar
-              title={sidebar.title}
-              description={sidebar.description}
-              archives={sidebar.archives}
-              social={sidebar.social}
-            />
           </Grid>
         </main>
       </Container>
