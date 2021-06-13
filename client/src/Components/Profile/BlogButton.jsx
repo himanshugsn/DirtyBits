4c55516fc243fa2f956312f8dc737f1ae03fc8e4
mohitb07 <bmohit980@gmail.com>
2021-06-13 21:29:43 +0530

    user blog

---
+++ client/src/Components/Profile/BlogButton.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import style from './blog.module.css'
+
+function BlogButton({date, title, para, avatar}) {
+  return (
+    <div className={style.blogwrapper}>
+    <div className={style.userBlogPost}>
+      <div className={style.imgPod}>
+        <img className={style.blogImg} src={avatar} alt="random"/>
+      </div>
+      <div className="container_copy">
+        <h3 className={style.blogh3}>{date}</h3>
+        <h1 className={style.blogh1}>{title}</h1>
+        <p className={style.blogp}>{para}</p>
+      </div>
+      <a className={style.btnPrimary} href='/blogs'>Read More</a>
+    </div>
+  </div>
+  )
+}
+
+export default BlogButton
