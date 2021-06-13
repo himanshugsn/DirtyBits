bd6b34977f6b0773f72e6b69a6bb9f2f912d3361
mohitb07 <bmohit980@gmail.com>
2021-06-13 21:32:28 +0530

    profile page implemented

--- client/src/Components/Profile/profile.jsx
+++ client/src/Components/Profile/profile.jsx
@@ -1,46 +1,76 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 // import styled, {css} from 'styled-components'
 import styles from './style.module.css'
 import BlogButton from './BlogButton'
+import axios from 'axios';
+import {PulseSpinner} from 'react-spinners-kit'
 
-function profile() {
-    const array = new Array(10).fill(10)
+
+function Profile() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [solved, setSolved] = useState(0)
+    const [attempted, setAttempted] = useState(0)
+    const [rank, setRank] = useState(0)
+    const [avatar, setAvatar] = useState('')
+    useEffect( () => {
+      async function getProfileData() {
+          const response = await axios.get('/api/current_user')
+          console.log(response.data)
+          setName(response.data.username)
+          setEmail(response.data.email)
+          setAvatar(response.data.avatar)
+          setRank(response.data.rank)
+          setSolved(response.data.solvedQuestion.length)
+          setAttempted(response.data.attemptedQuestions.length)
+      }
+      getProfileData()
+    }, [name])
+    const array = new Array(3).fill(3)
     return (
         <div className={styles.profileContainer}>
            <section className={styles.profile}>
                 <header className={styles.header}>
                     <div className={styles.details}>
-                    <img src="https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-0.3.5&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjE0NTg5fQ&s=b38c22a46932485790a3f52c61fcbe5a" alt="John Doe" className={styles.profilePic} />
-                    <h1 className={styles.heading}>Mohit Bisht</h1>
+                    {
+                        avatar ? <img src={avatar} alt="profile pic" className={styles.profilePic} /> : 
+                        <div className={styles.profileLoader}>
+                            <PulseSpinner />
+                        </div>
+                    }
+                        
+                    
+                    <h1 className={styles.heading}>{name}</h1>
                     <div className={styles.location}>
-                        <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M12,11.5A2.5,2.5 0 0,1 9.5,9A2.5,2.5 0 0,1 12,6.5A2.5,2.5 0 0,1 14.5,9A2.5,2.5 0 0,1 12,11.5M12,2A7,7 0 0,0 5,9C5,14.25 12,22 12,22C12,22 19,14.25 19,9A7,7 0 0,0 12 ,2Z"></path>
-                </svg>
-                        <p>Kochi, India</p>
+                        <p>{email}</p>
                     </div>
                     <div className={styles.stats}>
                         <div className={styles.col4}>
-                        <h4>20</h4>
-                        <p>Reviews</p>
+                        <h4>{solved}</h4>
+                        <p>Solved</p>
                         </div>
                         <div className={styles.col4}>
-                        <h4>10</h4>
-                        <p>Communities</p>
+                        <h4>{attempted}</h4>
+                        <p>Attempted</p>
                         </div>
                         <div className={styles.col4}>
-                        <h4>100</h4>
-                        <p>Discussions</p>
+                        <h4>{rank}</h4>
+                        <p>Rank</p>
                         </div>
                     </div>
                     </div>
                 </header>
                 </section>
+                <h2 style={{textAlign:'center', marginTop:'5rem'}}>My Blogs</h2>
                 <div className={styles.blogs}>
-                    {array.map(item => <div style={{margin:'10px'}}><BlogButton/></div>)}
+                    <div style={{margin:'3rem'}}><BlogButton avatar={avatar} date={'13 June 2021'} title={'React vs Vue'} para={'The primary difference lies in the methods used by Vue vs. React for rendering content onto the DOM.'}/></div>
+                    <div style={{margin:'3rem'}}><BlogButton avatar={avatar} date={'10 June 2021'} title={'Node Js (API)'} para={'the Express framework, and MongoDB, focusing on the fundamental REST routes and basic database interaction. '}/></div>
+                    <div style={{margin:'3rem'}}><BlogButton avatar={avatar} date={'1 June 2021'} title={'Microservices'} para={'It is an architectural style that structures an application as a collection of services'}/></div>
+
                 </div>
         </div>
     )
 }
 
 
-export default profile
+export default Profile
